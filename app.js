7a24f9292a25e4444a942fb3e1b9db7ea2b7d985
JadTahaGit <jadtaha.de@gmail.com>
2023-09-13 16:46:29 +0200

    Api Server configured package and .env

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get(
 
 // app.get("/api/income/:month/:year", )
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
