c76ca69272500b82d893d557530d06d82d511b1f
Jad Taha <Jadtaha.de@gmail.com>
2024-03-22 08:46:45 +0100

    last version on the Zen Mac

--- Cat_utlities/catAssigner.js
+++ Cat_utlities/catAssigner.js
@@ -4,9 +4,11 @@ const wordCategoryMapping = {
   "Studierendenwerk Muen": "Rent",
   Miete: "Rent",
   "SALON PRINZ": "Haircut",
+  Friseursalo: "Haircut",
   Haircut: "Haircut",
   //Supermarket
   ROSSMANN: "Supermarket",
+  "Al Arabi Markt Munchen GIR 69232329//MUNCHEN/DE": "Supermarket",
   "DM FIL": "Supermarket",
   REWE: "Supermarket",
   EDEKA: "Supermarket",
@@ -17,21 +19,34 @@ const wordCategoryMapping = {
   "ALDI SE": "Supermarket",
   "ERNST MUC HBF": "Supermarket",
   "BAECKEREI OGGI": "Supermarket",
+  "Kaufland Bamberg": "Supermarket",
+  "Kaufland Groebenzell": "Supermarket",
+  YORMAS: "Supermarket",
   //Resturants
   "DEAN DAVID": "Resturaunts",
+  "Sara-H Restaurant GmbH//Munchen/DE": "Resturaunts",
   "HELEIHEL HASSANEIN": "Resturaunts",
   "Honghong Noodles": "Resturaunts",
   MCDONALDS: "Resturaunts",
   Theresienwiese: "Resturaunts",
   SAUSALITOS: "Resturaunts",
   HAMBURGEREI: "Resturaunts",
+  "Casa Italia Bamberg": "Resturaunts",
+  "Gastronomie UG//Forchheim": "Resturaunts",
+  "Sindbad//Muenchen/DE": "Resturaunts",
+  "DIBRAN COCAJ": "Resturaunts",
+
   //Shoping
   Vodafone: "Phone",
   EBAY: "Shoping",
   AMAZON: "Shoping",
+  "tkmaxx-de/Peter-Mller-Str. 18,/Dsseldorf/DE": "Shoping",
+  "H+M Muenchen 067//Muenchen/DE": "Shoping",
   //Freetime & Fun
   AMZNPrime: "Freetime",
   kino: "Freetime",
+  "Astor im ARRI": "Freetime", //kino
+  "SCHLOSS- UND GARTENVERWALTUNG//PRIEN/DE": "Freetime",
   "Mastercard 5299": "Credit Card",
   "5299 5270 0511 6505": "Credit Card",
   //Transport
@@ -51,6 +66,7 @@ const wordCategoryMapping = {
   KVR: "State",
   Bundeskasse: "State",
   "EIGENE KREDITKARTENABRECHN.": "Sparkasse",
+  "Rechnung Löschung Überweisungsauftr": "Sparkasse",
   //Software&BussinessInvestment
   FIVERR: "Software&BussinessInvestment",
   "MO-Editor": "Software&BussinessInvestment",
@@ -65,8 +81,10 @@ const wordCategoryMapping = {
   SATURN: "Software&BussinessInvestment",
   //Stocks
   "TRADE REPUBLIC": "stocks",
+  "Trade Republic": "stocks",
   //Health&Sport
   "FIT STAR": "Health&Sport",
+  "EGYM Wellpass GmbH": "Health&Sport",
   MICHAELIBAD: "Health&Sport",
   APOTHEKE: "Health&Sport",
   "TOWASAN GmbH": "Health&Sport",
@@ -80,6 +98,8 @@ const wordCategoryMapping = {
   "Hamza Ahmed": "Education",
   Digistore24: "Education",
   CopeCart: "Education",
+  "Marc Galal GmbH": "Education",
+  "1030293450357/PP.2985.PP/. , Ihr Einkauf bei": "Education", //Gabreal Youtube Education
   //Charity
   "BRK-KV Muenchen": "Charity",
   //Travel
@@ -88,10 +108,13 @@ const wordCategoryMapping = {
   PEGASUS: "Travel",
   Pegasus: "Travel",
   "YEVHENIIA SKOK": "Travel",
+  Skok: "Travel",
   "ALI AL-DARRAJI": "Travel",
   "ali al daraji": "Travel",
   "Miriam Matisova": "Travel",
+  "Airbnb Payments Luxembourg ": "Travel",
   "MAKA RAZMADZE": "Friends",
+  "Maha Saied Mohammed Mohammed Elsaied": "Friends",
   "YESSICA MOESSNER": "Friends",
   "Somaya Abdallah-Zeidan": "Friends",
   "JUNFENG CHEN": "Friends",
@@ -103,7 +126,10 @@ const wordCategoryMapping = {
   PayPal: "PayPal",
   //Saving
   karimooo: "Saving",
+  "Karimo Savings": "Saving",
   "Sparkonto,3mon.Kuendig.": "Saving",
+  "MoneyGram Retail//Muenchen/DE": "Karimo's Schulden Begleichen",
+  "STUDIENKREDIT DA19705535 AUSZAHLUNG": "Schulden Begleichen",
 };
 
 const assignCategories = (transactions) => {
@@ -118,7 +144,10 @@ const assignCategories = (transactions) => {
       category = "Bar Geld Einzahlung";
     } else if (bookingText.includes("UMBUCHUNG")) {
       category = "Umbuchung";
-    } else if (bookingText.includes("UEBERTRAG (UEBERWEISUNG)")) {
+    } else if (
+      bookingText.includes("UEBERTRAG (UEBERWEISUNG)") ||
+      bookingText.includes("UEBERTRAG (GUTSCHR. UEBERW)")
+    ) {
       category = "Umbuchung";
     } else if (bookingText.includes("BARGELDAUSZAHLUNG")) {
       // For the case when haircut inserted manually
@@ -131,9 +160,14 @@ const assignCategories = (transactions) => {
       category = "Credit Card";
     } else if (bookingText.includes("ABSCHLUSS")) {
       category = "Sparkasse";
-    } else if (
+    }
+    //paypal bussiness Income
+    else if (payerReceiver && payerReceiver.includes("PayPal") && amount > 0) {
+      category = "Bussines Income";
+    } //Direct bussiness Income
+    else if (
       payerReceiver &&
-      payerReceiver.includes("PayPal") &&
+      payerReceiver.includes("MAKA RAZMADZE") &&
       amount > 0
     ) {
       category = "Bussines Income";
